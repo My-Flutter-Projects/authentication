b19d86941aec1237ba0dd0bb76b4e29c0ddd5e9b
shahnawazsalsoft <shahnawazsalsoft@gmail.com>
2022-07-19 17:29:02 +0500

    Login screen and assets

---
+++ lib/screens/login.dart
@@ -0,0 +1,114 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/login.png"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Container(
+              // margin: Margin,
+              padding: EdgeInsets.only(
+                left: 30,
+                top: 125,
+              ),
+              child: Text(
+                "Welcome \nBack",
+                style: TextStyle(color: Colors.white, fontSize: 33),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.5,
+                  left: 35,
+                  right: 35,
+                ),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: "Email",
+                        fillColor: Colors.grey.shade100,
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        fillColor: Colors.grey.shade100,
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Sign In",
+                            style: TextStyle(
+                              fontSize: 27,
+                              color: Color(0xff4c505b),
+                              fontWeight: FontWeight.w700,
+                            )),
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundColor: Color(0xff4c505b),
+                          child: IconButton(
+                              color: Colors.white,
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.arrow_forward,
+                              )),
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text("Sign Up",
+                              style: TextStyle(
+                                color: Colors.red,
+                              )),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
